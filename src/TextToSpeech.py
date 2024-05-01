9759a2ad1aa79357f170ea3b415d3df37f3dfacc
furaga <furaga.fukahori@gmail.com>
2024-05-01 21:57:39 +0900

    voicevox_urlをちゃんと使う

--- src/TextToSpeech.py
+++ src/TextToSpeech.py
@@ -3,18 +3,19 @@ import json
 
 
 class TextToSpeech:
-    def __init__(self, voicevox_url="http://127.0.0.1:50021"):
+    def __init__(self, voicevox_url="http://59.147.132.97:50021"):
+#    def __init__(self, voicevox_url="http://127.0.0.1:50021"):
         self.voicevox_url = voicevox_url
 
     def tts(self, text, speed=1.1, speaker=3):
         res1 = requests.post(
-            "http://127.0.0.1:50021/audio_query",
+            self.voicevox_url + "/audio_query",
             params={"text": text, "speaker": speaker},
         )
         data = res1.json()
         data["speedScale"] = speed
         res2 = requests.post(
-            "http://127.0.0.1:50021/synthesis",
+            self.voicevox_url + "/synthesis",
             params={"speaker": speaker},
             data=json.dumps(data),
         )
